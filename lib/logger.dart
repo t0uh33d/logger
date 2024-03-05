7e31bb22f044c40ea56fa319215eaf16a9e92e25
Touheed Khan <touheed@codewave.com>
2024-03-05 14:53:18 +0530

    first comit

---
+++ lib/logger.dart
@@ -0,0 +1,6 @@
+/// Small, easy to use and extensible logger which prints beautiful logs.
+library logger;
+
+export 'src/outputs/file_output_stub.dart'
+    if (dart.library.io) 'src/outputs/file_output.dart';
+export 'web.dart';
